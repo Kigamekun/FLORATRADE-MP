9d11757cec4ff5fcd949166fff1b103f1f93d63b
yukivh <abdurrahmanr2322@gmail.com>
2025-05-16 09:43:59 +0700

    profile tran history fix

--- resources/views/user/history-transaksi.blade.php
+++ resources/views/user/history-transaksi.blade.php
@@ -5,7 +5,7 @@
     <link rel="stylesheet" href="{{ url('assets_user/css/history-transaksi.css') }}">
     <link rel="stylesheet" href="{{ url('assets_user/css/checkout.css') }}">
     <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Dropify/0.2.2/css/dropify.css"
         integrity="sha512-In/+MILhf6UMDJU4ZhDL0R0fEpsp4D3Le23m6+ujDWXwl3whwpucJG1PEmI3B07nyJx+875ccs+yX2CqQJUxUw=="
         crossorigin="anonymous" referrerpolicy="no-referrer">
@@ -384,19 +384,24 @@
                         <hr class="divider">
                         <nav class="sidebar-nav">
                             <a href="{{ route('profile') }}" class="nav-item active">
-                                <i class="fas fa-user nav-icon"></i>
+                                <img src="{{ asset('assets_user/img/icon/user-solid.svg') }}" alt="profile" class="nav-icon">
                                 <span class="nav-text">Account</span>
                             </a>
+                            <!--
                             <a href="{{ route('chat', ['for' => 1]) }}" class="nav-item">
-                                <i class="fas fa-comment-dots nav-icon"></i>
+                                <img src="{{ asset('assets_user/img/icon/cart-shopping-solid.svg') }}" alt="profile" class="nav-icon">
                                 <span class="nav-text">Chat</span>
                             </a>
+                            -->
                             <a href="{{ route('history-transaction') }}" class="nav-item">
-                                <i class="fas fa-shopping-cart nav-icon"></i>
+                                <img src="{{ asset('assets_user/img/icon/cart-shopping-solid.svg') }}" alt="profile" class="nav-icon">
                                 <span class="nav-text">History Transaction</span>
                             </a>
                             <a href="{{ route('logoutUser') }}" class="nav-item logout">
-                                <i class="fas fa-sign-out-alt nav-icon"></i>
+                                <svg class="nav-icon" viewBox="0 0 512 512" width="19" height="19" fill="red" xmlns="http://www.w3.org/2000/svg">
+  <!-- Font Awesome Free 6.4.0 sign-out-alt icon (converted to inline SVG) -->
+  <path d="M160 416H96c-17.67 0-32-14.33-32-32V128c0-17.67 14.33-32 32-32h64c17.67 0 32-14.33 32-32S177.7 32 160 32H96C42.98 32 0 74.98 0 128v256c0 53.02 42.98 96 96 96h64c17.67 0 32-14.33 32-32S177.7 416 160 416zM502.6 233.4l-128-128c-12.51-12.51-32.76-12.49-45.25 0c-12.5 12.5-12.5 32.75 0 45.25L402.8 224H192C174.3 224 160 238.3 160 256s14.31 32 32 32h210.8l-73.38 73.38c-12.5 12.5-12.5 32.75 0 45.25s32.75 12.5 45.25 0l128-128C515.1 264.2 515.1 247.8 502.6 233.4z"/>
+</svg>
                                 <span class="nav-text">Logout</span>
                             </a>
                         </nav>
